df77d62c49c275ee10cbb3521a666093f7e4bf24
Rahichauhan <rahichauhan36@gmail.com>
2025-01-05 22:25:39 +0530

    remove

--- node/db/conn.js
+++ node/db/conn.js
@@ -1,6 +1,5 @@
 const mongoose=require("mongoose");
 mongoose.connect("mongodb+srv://rahichauhan37:Rahi12345@cluster0.judhvvw.mongodb.net/fashion",{
-    useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(()=>{
     console.log("Connected to Mongodb");
